5833218ec6d024f78f071a1f0cabbe75fb55ccce
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-04-08 10:24:04 -0600

    Capa Logica de negocios terminada.

---
+++ L20240408.LogicaDeNegocios/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using L20240408.AccesoADatos;
+using L20240408.EntidadesDeNegocios;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20240408.LogicaDeNegocios
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDalDependecies(configuration);
+            services.AddScoped<PersonaLBL>();
+            return services;
+        }
+    }
+}
---
+++ L20240408.LogicaDeNegocios/PersonaLBL.cs
@@ -0,0 +1,44 @@
+﻿using L20240408.AccesoADatos;
+using L20240408.EntidadesDeNegocios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20240408.LogicaDeNegocios
+{
+    public class PersonaLBL
+    {
+        readonly PersonaLDAL _personaDAL;
+
+        public PersonaLBL(PersonaLDAL personaDAL)
+        {
+            _personaDAL = personaDAL;
+        }
+        public async Task<int> Crear(PersonaL personaL)
+        {
+            return await _personaDAL.Crear(personaL);
+        }
+        public async Task<int> Modificar(PersonaL personaL)
+        {
+            return await _personaDAL.Modificar(personaL);
+        }
+        public async Task<int> Eliminar(PersonaL personaL)
+        {
+            return await _personaDAL.Eliminar(personaL);
+        }
+        public async Task<PersonaL> ObtenerPoId(PersonaL personaL)
+        {
+            return await _personaDAL.ObtenerPoId(personaL);
+        }
+        public async Task<List<PersonaL>> ObtenerTodos()
+        {
+            return await _personaDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonaL>> Buscar(PersonaL personaL)
+        {
+            return await _personaDAL.Buscar(personaL);
+        }
+    }
+}
